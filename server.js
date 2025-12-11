2de7ee42e6cdc8641be8352a34b42390e2530abb
Sudarsan Rajasekar <107539573+Sudarsan-Rajasekar@users.noreply.github.com>
2025-12-11 16:43:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ app.get("/getPrice/:item", async (req, res) => {
 
 app.post("/webhook/order", async(req,res)=>{
   try{
-    console.log("Incoming order:", req.body.data_collection_results);
+    console.log("Incoming order:", req.body.analysis.data_collection_results);
     const{item, price} = req.body
 
     if (!item || !price) {
@@ -244,4 +244,5 @@ app.listen(PORT, () => {
   console.log(`✅ Café Buddy running at http://localhost:${PORT}`);
 });
  
+
 
